3be259b9a8df6bc5fab208fb70d8990c1933d457
Logan Gray <logan@intensetech.com>
2022-08-10 11:40:01 -0700

    Added removeCert function to delete top one only.   Changed it to use 2 params, added removebatch param

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -566,7 +566,50 @@ else
 		;;
 	esac
 	}
+function removeCert ()
+{
+	#This currently will only delete the 1st client... for testing ;)
+	number_of_clients=$(tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep -c "^V")
+			if [[ "$number_of_clients" = 0 ]]; then
+				echo
+				echo "There are no existing clients!"
+				exit
+			fi
+			echo
+			echo "Select the client to revoke:"
+			#this lists clients
+			tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | nl -s ') '
 
+			# read -p "Client: " client_number
+			# until [[ "$client_number" =~ ^[0-9]+$ && "$client_number" -le "$number_of_clients" ]]; do
+			# 	echo "$client_number: invalid selection."
+			# 	read -p "Client: " client_number
+			# done
+			client_number=1
+
+			client=$(tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | sed -n "$client_number"p)
+			echo
+			# read -p "Confirm $client revocation? [y/N]: " revoke
+			# until [[ "$revoke" =~ ^[yYnN]*$ ]]; do
+			# 	echo "$revoke: invalid selection."
+			# 	read -p "Confirm $client revocation? [y/N]: " revoke
+			# done
+			# if [[ "$revoke" =~ ^[yY]$ ]]; then
+				cd /etc/openvpn/server/easy-rsa/
+				./easyrsa --batch revoke "$client"
+				EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
+				rm -f /etc/openvpn/server/crl.pem
+				cp /etc/openvpn/server/easy-rsa/pki/crl.pem /etc/openvpn/server/crl.pem
+				# CRL is read with each client connection, when OpenVPN is dropped to nobody
+				chown nobody:"$group_name" /etc/openvpn/server/crl.pem
+				echo
+				echo "$client revoked!"
+			# else
+			# 	echo
+			# 	echo "$client revocation aborted!"
+			# fi
+			# exit
+}
 function addUser ()
 {
 			username2be=$1
@@ -598,11 +641,43 @@ if [ $# -eq 0 ]
 	askOptions
 	exit 1
 fi
-if [ $# -eq 1 ]
+if [ $# -eq 2 ] && [ $1 == "adduser" ]
   then
-    addUser $$1
+  	echo "Adding a user..."
+    addUser $$2
 	exit 1
 fi
+if [ $# -eq 2 ] && [ $1 == "remove" ]
+  then
+	echo "Removing User #1..."
+    removeCert
+	exit 1
+fi
+if [ $# -eq 2 ] && [ $1 == "removebatch" ]
+  then
+	tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | nl -s ') '
+	echo "Removing a batch of users... essentailly 1 to $2"
+	while true; do
 
+	read -p "Are you absolutely sure? (yes/no) " yn
+
+	case $yn in 
+	yes ) echo ok, we will proceed;
+		break;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+	esac
+	done
+
+	i=0
+	while [ $i -lt $2 ]
+	do echo i: $i
+	((i++))
+	removeCert
+	done
+    # 
+	exit 1
+fi
 
 fi
